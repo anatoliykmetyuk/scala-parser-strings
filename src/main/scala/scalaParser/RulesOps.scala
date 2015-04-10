09af3492286155ce2cb4b01a5ec6facc1103f2c4
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-04-10 12:17:19 +0300

    RulesOps added

---
+++ src/main/scala/scalaParser/RulesOps.scala
@@ -0,0 +1,20 @@
+package scalaParser
+
+trait RulesOps {
+
+  type S = String
+
+  def Concat : (S, S)                => S = _+_
+  def Concat3: (S, S, S)             => S = _+_+_
+  def Concat4: (S, S, S, S)          => S = _+_+_+_
+  def Concat5: (S, S, S, S, S)       => S = _+_+_+_+_
+  def Concat7: (S, S, S, S, S, S, S) => S = _+_+_+_+_+_+_
+
+  def ConcatSeq(delimiter: String): (Seq[String]) => String = _.mkString(delimiter)
+  def ConcatSeqSemi    = ConcatSeq(";")
+  def ConcatSeqNoDelim = ConcatSeq("" )
+  def ConcatSeqComma   = ConcatSeq(",")
+
+  def ExtractOpt: Option[String] => String = _.getOrElse("")
+
+}
